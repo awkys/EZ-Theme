85163638d8bfbab7422906ecb252a15d85d9c5a4
awkys <98374805+awkys@users.noreply.github.com>
2025-10-27 17:52:56 +0800

    1
    1

--- src/config/index.js
+++ src/config/index.js
@@ -30,7 +30,7 @@ export const config = {
     // 多个地址时，会按顺序检测可用性，并使用第一个可用的地址
     staticBaseUrl: [
       //   "https://bun.xn--bwwx30f.top/bunsurf/subscribe",
-      "https://vb2.bunsurf.xyz/bunsurf/subscribe",
+      "https://vb2.bunsurf.xyz/api/v1",
     ],
 
     // 自动获取模式配置 (urlMode = 'auto'时使用)
@@ -42,8 +42,8 @@ export const config = {
       appendApiPath: true,
 
       // API路径
-      //   apiPath: "/api/v1",
-      apiPath: "/bunsurf/subscribe",
+      apiPath: "/api/v1",
+      //   apiPath: "/bunsurf/subscribe",
     },
   },
 
