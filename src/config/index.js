a4a5dd40d4029e3363197ccef981af35114ba00f
awkys <98374805+awkys@users.noreply.github.com>
2025-10-31 09:36:02 +0800

    中间件

--- src/config/index.js
+++ src/config/index.js
@@ -32,7 +32,7 @@ export const config = {
       // "https://bun.xn--bwwx30f.top/api/v1",
       // "https://vip.bunsurf.xyz/api/v1",
       // "https://vb2.bunsurf.xyz/api/v1",
-      "https://vb.yercat.com/api/v1",
+      // "https://vb.yercat.com/api/v1",
       //   "http://127.0.0.1/api/v1",
     ],
 
@@ -52,20 +52,21 @@ export const config = {
 
   // 是否启用中间件代理API请求
   // 设置为true时，所有API请求将通过中间件转发
-  //   API_MIDDLEWARE_ENABLED: true,
-  API_MIDDLEWARE_ENABLED: false,
+  API_MIDDLEWARE_ENABLED: true,
+  // API_MIDDLEWARE_ENABLED: false,
 
   //=======================================================
   // 中间件服务器URL (不含路径) 开源地址 https://github.com/codeman857/EZ-Encrypt-Middleware
   //   API_MIDDLEWARE_URL: "https://d2ijw202als7c.ezdemo.xyz",
-  API_MIDDLEWARE_URL: "https://vb2.bunsurf.xyz",
+  // API_MIDDLEWARE_URL: "https://vb2.bunsurf.xyz",
+  API_MIDDLEWARE_URL: "http://127.0.0.1:3000",
 
   // 中间件加密KEY必须是16位的16进制字符串，必须和中间件key保持一致 在线生成地址 https://www.bejson.com/math/hex_gen/
-  API_MIDDLEWARE_KEY: "4c6f8e5f9467dc71",
+  API_MIDDLEWARE_KEY: "5ba2574efbf1232b",
   //=======================================================
 
   // 中间件路由前缀 (与中间件服务器配置保持一致)
-  API_MIDDLEWARE_PATH: "/ez/ez",
+  API_MIDDLEWARE_PATH: "/ez/ezreirhik",
 
   //=======================================================
 
