aa110d8b62f40affd3adc0eb5bf5e12a975167cc
awkys <98374805+awkys@users.noreply.github.com>
2025-11-15 08:05:45 +0800

    邀请链接

--- src/config/index.js
+++ src/config/index.js
@@ -456,7 +456,7 @@ export const config = {
       // 链接模式：'auto'=自动使用当前站点域名，'custom'=使用自定义域名
       linkMode: "custom",
       // 自定义域名，当linkMode为'custom'时使用
-      customDomain: "https://bun.数字环保.shop",
+      customDomain: "https://bun.cetkny.store",
     },
   },
 
