f021bd60b5efa3604d66eb2fbdd85fae1b5edc51
awkys <98374805+awkys@users.noreply.github.com>
2026-07-13 13:10:56 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -293,10 +293,10 @@ export const config = {
     clientLinks: {
       ios: "https://zt.bunsurf.store/#/docs/10",
       android:
-        "https://down.nxkys.com/client/FlClash-v1.2.0-android-universal.apk",
-      macos: "https://down.nxkys.com/client/FlClash-v1.2.0-macos-universal.dmg",
+        "https://down.nxkys.com/client/FlClash-v2.0.1-android-universal.apk",
+      macos: "https://down.nxkys.com/client/FlClash-v2.0.1-macos-universal.dmg",
       windows:
-        "https://down.nxkys.com/client/FlClash-v1.2.0-windows-x64-setup.exe",
+        "https://down.nxkys.com/client/FlClash-v2.0.1-windows-x64-setup.exe",
       linux: "https://github.com/xxx/releases/latest",
       openwrt: "https://github.com/xxx/releases/latest",
     },
